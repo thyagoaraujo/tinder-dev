d76f788afc851eef5443751cf0105d629a5e85ee
Thyago Araujo <thyagopcb.araujo@gmail.com>
2019-08-07 12:45:08 -0300

    Listing devs

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -2,6 +2,26 @@ const axios = require('axios');
 const Dev = require('../models/Dev');
 
 module.exports = {
+  async index(req, res) {
+    const { user } = req.headers;
+
+    const loggedDev = await Dev.findById(user);
+
+    /**
+     * retorna usuários que não seja o usuário logado, e não esteja em likes e
+     * dislikes */
+
+    const users = await Dev.find({
+      $and: [
+        { _id: { $ne: user } },
+        { _id: { $nin: loggedDev.likes } },
+        { _id: { $nin: loggedDev.dislikes } }
+      ]
+    });
+
+    return res.json(users);
+  },
+
   async store(req, res) {
     const { username } = req.body;
 
