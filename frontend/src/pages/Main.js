47381baac5e7cedf7cb84242448e2aa51e26f322
Thyago Araujo <thyagopcb.araujo@gmail.com>
2019-08-07 22:40:44 -0300

    add handleDislike

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -21,6 +21,18 @@ export default function Main({ match }) {
     loadUsers();
   }, [match.params.id]);
 
+  async function handleLike(id) {
+    console.log('like', id);
+  }
+
+  async function handleDislike(id) {
+    await api.post(`/devs/${id}/dislikes`, null, {
+      headers: { user: match.params.id }
+    });
+
+    setUsers(users.filter(user => user._id !== id));
+  }
+
   return (
     <div className="main-container">
       <img src={logo} alt="Tindev" />
@@ -34,10 +46,10 @@ export default function Main({ match }) {
             </footer>
 
             <div className="buttons">
-              <button type="button">
+              <button type="button" onClick={() => handleDislike(user._id)}>
                 <img src={dislike} alt="Dislike" />
               </button>
-              <button type="button">
+              <button type="button" onClick={() => handleLike(user.id)}>
                 <img src={like} alt="Like" />
               </button>
             </div>
