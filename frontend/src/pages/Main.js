e049c073fbe89199a4fd9993fa4e2a2e0162d55e
Thyago Araujo <thyagopcb.araujo@gmail.com>
2019-08-07 22:20:31 -0300

    get users data and show in grid

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,96 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Main.css';
-//import api from '../services/api';
+import api from '../services/api';
 import logo from '../assets/logo.svg';
 import dislike from '../assets/dislike.svg';
 import like from '../assets/like.svg';
 
 export default function Main({ match }) {
-  /*useEffect(() => {
-    async function loadUsers() {}
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function loadUsers() {
+      const response = await api.get('/devs', {
+        headers: {
+          user: match.params.id
+        }
+      });
+
+      setUsers(response.data);
+    }
     loadUsers();
-  }, [match.params.id]);*/
+  }, [match.params.id]);
 
   return (
     <div className="main-container">
       <img src={logo} alt="Tindev" />
       <ul>
-        <li>
-          <img
-            src="https://avatars0.githubusercontent.com/u/4248081?v=4"
-            alt=""
-          />
-          <footer>
-            <strong>Filipe Deschamps</strong>
-            <p>Programador e cantor sertanejo nos tempos livres.</p>
-          </footer>
+        {users.map(user => (
+          <li key={user._id}>
+            <img src={user.avatar} alt={user.name} />
+            <footer>
+              <strong>{user.name}</strong>
+              <p>{user.bio}</p>
+            </footer>
 
-          <div className="buttons">
-            <button type="button">
-              <img src={dislike} alt="Dislike" />
-            </button>
-            <button type="button">
-              <img src={like} alt="Like" />
-            </button>
-          </div>
-        </li>
-        <li>
-          <img
-            src="https://avatars0.githubusercontent.com/u/4248081?v=4"
-            alt=""
-          />
-          <footer>
-            <strong>Filipe Deschamps</strong>
-            <p>Programador e cantor sertanejo nos tempos livres.</p>
-          </footer>
-
-          <div className="buttons">
-            <button type="button">
-              <img src={dislike} alt="Dislike" />
-            </button>
-            <button type="button">
-              <img src={like} alt="Like" />
-            </button>
-          </div>
-        </li>
-        <li>
-          <img
-            src="https://avatars0.githubusercontent.com/u/4248081?v=4"
-            alt=""
-          />
-          <footer>
-            <strong>Filipe Deschamps</strong>
-            <p>Programador e cantor sertanejo nos tempos livres.</p>
-          </footer>
-
-          <div className="buttons">
-            <button type="button">
-              <img src={dislike} alt="Dislike" />
-            </button>
-            <button type="button">
-              <img src={like} alt="Like" />
-            </button>
-          </div>
-        </li>
-        <li>
-          <img
-            src="https://avatars0.githubusercontent.com/u/4248081?v=4"
-            alt=""
-          />
-          <footer>
-            <strong>Filipe Deschamps</strong>
-            <p>Programador e cantor sertanejo nos tempos livres.</p>
-          </footer>
-
-          <div className="buttons">
-            <button type="button">
-              <img src={dislike} alt="Dislike" />
-            </button>
-            <button type="button">
-              <img src={like} alt="Like" />
-            </button>
-          </div>
-        </li>
+            <div className="buttons">
+              <button type="button">
+                <img src={dislike} alt="Dislike" />
+              </button>
+              <button type="button">
+                <img src={like} alt="Like" />
+              </button>
+            </div>
+          </li>
+        ))}
       </ul>
     </div>
   );
