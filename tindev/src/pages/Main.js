fc7ace964720d87d88c74145e83fb1da151b10ea
Thyago Araujo <thyagopcb.araujo@gmail.com>
2019-08-09 16:06:35 -0300

    add handleLogout

--- tindev/src/pages/Main.js
+++ tindev/src/pages/Main.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
 import api from '../services/api';
 import {
   View,
@@ -45,10 +46,17 @@ export default function Main({ navigation }) {
     setUsers(users.filter(user => user._id !== id));
   }
 
+  async function handleLogout() {
+    await AsyncStorage.clear();
+
+    navigation.navigate('Login');
+  }
+
   return (
     <SafeAreaView style={styles.container}>
-      <Image style={styles.logo} source={logo} />
-
+      <TouchableOpacity onPress={handleLogout}>
+        <Image style={styles.logo} source={logo} />
+      </TouchableOpacity>
       <View style={styles.cardsContainer}>
         {users.length === 0 ? (
           <Text style={styles.empty}>Acabou :(</Text>
