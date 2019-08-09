d102031f33f0aed7b03ae246e456cef7e8ab7af6
Thyago Araujo <thyagopcb.araujo@gmail.com>
2019-08-09 16:19:55 -0300

    add handleLike and handleDislike

--- tindev/src/pages/Main.js
+++ tindev/src/pages/Main.js
@@ -30,20 +30,24 @@ export default function Main({ navigation }) {
     loadUsers();
   }, [id]);
 
-  async function handleLike(id) {
-    await api.post(`/devs/${id}/likes`, null, {
+  async function handleLike() {
+    const [user, ...rest] = users;
+
+    await api.post(`/devs/${user._id}/likes`, null, {
       headers: { user: id }
     });
 
-    setUsers(users.filter(user => user._id !== id));
+    setUsers(rest);
   }
 
-  async function handleDislike(id) {
-    await api.post(`/devs/${id}/dislikes`, null, {
+  async function handleDislike() {
+    const [user, ...rest] = users;
+
+    await api.post(`/devs/${user._id}/dislikes`, null, {
       headers: { user: id }
     });
 
-    setUsers(users.filter(user => user._id !== id));
+    setUsers(rest);
   }
 
   async function handleLogout() {
@@ -79,11 +83,11 @@ export default function Main({ navigation }) {
       </View>
 
       <View style={styles.buttonsContainer}>
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity style={styles.button} onPress={handleDislike}>
           <Image source={dislike} />
         </TouchableOpacity>
 
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity style={styles.button} onPress={handleLike}>
           <Image source={like} />
         </TouchableOpacity>
       </View>
